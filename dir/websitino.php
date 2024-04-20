b26475fe72358b0e6f8a4a7f5b5dcbb37b43d10b
Ehsan Ghafoori <118250253+EhsanGhafoori@users.noreply.github.com>
2024-04-20 23:47:15 +0400

    full translate but not working

--- dir/websitino.php
+++ dir/websitino.php
@@ -1,20 +1,30 @@
 <?php
-require_once plugin_dir_path( __FILE__ ) . "styles.php";
-require_once plugin_dir_path( __FILE__ ) . "loginStyle.php";
-require_once plugin_dir_path( __FILE__ ) . "footer.php";
+require_once plugin_dir_path(__FILE__) . "styles.php";
+require_once plugin_dir_path(__FILE__) . "loginStyle.php";
+require_once plugin_dir_path(__FILE__) . "footer.php";
 
-require_once plugin_dir_path( __FILE__ ) . "first-page.php";
+require_once plugin_dir_path(__FILE__) . "first-page.php";
 
-function websitino_custom_css() {   echo '<style>
+function websitino_custom_css()
+{
+	echo '<style>
 div#llar-header-upgrade-message {
     display: none;
 }
-</style>'; 
-} 
+</style>';
+}
 add_action('admin_head', 'websitino_custom_css');
 
-function websitino_remove_admin_footer(){
-echo '<span id="footer-websitino-th">سپاسگزاریم از اینکه سایت خود را با <a href="https://websitino.com/">وبسایتینو</a> ساخته‌اید.</span>';
+function websitino_remove_admin_footer()
+{
+?>
+	<span id="footer-websitino-th">
+		<?php _e("Thank you for creating with", "websitino") ?>
+		<a href="https://websitino.com/"><?php _e("Websitino", "websitino") ?></a>
+		.
+	</span>
+
+<?php
 }
 add_filter('admin_footer_text', 'websitino_remove_admin_footer');
 
@@ -23,24 +33,21 @@ add_filter('admin_footer_text', 'websitino_remove_admin_footer');
 
 
 // Load the theme stylesheets
-function websitino_theme_styles()  
-{ 
-	wp_enqueue_style( 'websitinoStyleFont', plugin_dir_url(__DIR__) . 'assets/css/fonts.css' );
-	wp_enqueue_style( 'bootstrapCss', plugin_dir_url(__DIR__) . 'assets/css/bootstrap.min.css' );
-    wp_style_add_data( 'bootstrapCss', 'rtl', plugin_dir_url(__DIR__) . 'assets/css/bootstrap.rtl.min.css' );
-    
-	wp_enqueue_style( 'bootstrapFeatures', plugin_dir_url(__DIR__) . 'assets/css/features.css' );
-	wp_enqueue_style( 'websitinoStyle', plugin_dir_url(__DIR__) . 'assets/css/style.css' );
+function websitino_theme_styles()
+{
+	wp_enqueue_style('websitinoStyleFont', plugin_dir_url(__DIR__) . 'assets/css/fonts.css');
+	wp_enqueue_style('bootstrapCss', plugin_dir_url(__DIR__) . 'assets/css/bootstrap.min.css');
+	wp_style_add_data('bootstrapCss', 'rtl', plugin_dir_url(__DIR__) . 'assets/css/bootstrap.rtl.min.css');
 
+	wp_enqueue_style('bootstrapFeatures', plugin_dir_url(__DIR__) . 'assets/css/features.css');
+	wp_enqueue_style('websitinoStyle', plugin_dir_url(__DIR__) . 'assets/css/style.css');
 }
 add_action('wp_enqueue_style', 'websitino_theme_styles');
 
-function websitino_theme_scripts()  
-{ 
+function websitino_theme_scripts()
+{
 
-	wp_enqueue_style( 'bootstrapJs', plugin_dir_url(__DIR__) . 'assets/js/bootstrap.bundle.min.js' );
-
-
+	wp_enqueue_style('bootstrapJs', plugin_dir_url(__DIR__) . 'assets/js/bootstrap.bundle.min.js');
 }
 add_action('wp_enqueue_scripts', 'websitino_theme_scripts');
 
@@ -53,7 +60,7 @@ add_action('admin_head', 'websitino_theme_styles');
 function author_add_meta_tags()
 {
 
-    echo '<meta name="author" content="Ehsan Ghafoori" />'. "\n";
+	echo '<meta name="author" content="Ehsan Ghafoori" />' . "\n";
 }
 
 
