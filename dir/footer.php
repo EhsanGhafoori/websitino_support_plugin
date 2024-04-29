731949e32aee71caec2684d8349d1a672fc6b2db
Ehsan Ghafoori <118250253+EhsanGhafoori@users.noreply.github.com>
2024-04-29 16:54:12 +0400

    add font logo websitino

--- dir/footer.php
+++ dir/footer.php
@@ -8,20 +8,35 @@ function websitino_footer_first_page()
 
     if (get_bloginfo("language") == 'en-US') {
 ?>
+<style>
+    
+@font-face {
+	font-family: Righteous-Regular;
+	font-style: normal;
+	font-weight: 500;
+	src: url('/wp-content/plugins/websitino_support_plugin-main/assets/fonts/ttf/Righteous-Regular.ttf');
+	src: url('/wp-content/plugins/websitino_support_plugin-main/assets/fonts/ttf/Righteous-Regular?#iefix') format('embedded-opentype');
+}
+
+.Righteous-Regular {
+    font-family: Righteous-Regular !important;
+}
+
+</style>
         <p style="border-style: solid;
         border-width: 1px 0px 0px 0px;
-        border-color: rgba(255, 255, 255, 0.1); text-align: center; padding-bottom:10px; padding-top:10px; margin-bottom:0px; font-family: 'Lexend Deca', Sans-serif !important; background-color:#272c30;">
+        border-color: rgb(139 139 139 / 10%); text-align: center; padding-bottom:10px; padding-top:10px; margin-bottom:0px; font-family: 'Lexend Deca', Sans-serif !important;  background-color:#ffffff;">
 
             All rights reserved for
 
             <a href="<?php echo site_url() ?>">
-                <span style="color: #ffffff;">
-                    <?php echo get_bloginfo(); ?>.
+                <span style="color: #000; font-weight: bold;">
+                    <?php echo strtoupper(get_bloginfo()) ;?>.
                 </span>
             </a>
             Created by
-            <span style="color: #ffffff; ">
-                <a style="color: #ffffff;" href="https://www.websitino.com/" target="_blank" rel="noopener">
+            <span style="color: #000; ">
+                <a style="color: #000; font-family:Righteous-Regular ;" href="https://www.websitino.com/" target="_blank" rel="noopener">
                     Websitino
                 </a>
             </span>
@@ -30,19 +45,19 @@ function websitino_footer_first_page()
     <?php
     } else {
     ?>
-        <p class="footer-container text-center" style=" border-top: 1px solid rgba(255,255,255,0.15); color:rgba(255,255,255,0.8); padding-bottom:10px; padding-top:10px; margin-bottom:0px; background-color:#272c30;">
+        <p class="footer-container text-center" style=" border-top: 1px solid rgba(255,255,255,0.15); color:rgba(255,255,255,0.8); padding-bottom:10px; padding-top:10px; margin-bottom:0px; background-color:#ffffff;">
 
             تمامی حقوق برای
             <a href="<?php echo site_url() ?>">
-                <span style="color: #ffffff;"><?php echo get_bloginfo(); ?>
+                <span style="color: #000;"><?php echo get_bloginfo(); ?>
                 </span>
             </a>
             محفوظ است.
 
 
             طراحی شده توسط
-            <span style="color: #ffffff; ">
-                <a style="color: #ffffff;" href="https://www.websitino.com/" target="_blank" rel="noopener">
+            <span style="color: #000; ">
+                <a style="color: #000;" href="https://www.websitino.com/" target="_blank" rel="noopener">
                     وبسایتینو
                 </a>
             </span>
