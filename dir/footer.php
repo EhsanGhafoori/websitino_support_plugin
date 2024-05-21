4d03bd8cefaef4237b6d45c2392a413360347ea0
Ehsan Ghafoori <118250253+EhsanGhafoori@users.noreply.github.com>
2024-05-21 23:27:06 +0400

    add arabic

--- dir/footer.php
+++ dir/footer.php
@@ -25,7 +25,7 @@ function websitinu_footer_first_page()
 </style>
         <p style="border-style: solid;
         border-width: 1px 0px 0px 0px;
-        border-color: rgb(139 139 139 / 10%); text-align: center; padding-bottom:10px; padding-top:10px; margin-bottom:0px; font-family: 'Lexend Deca', Sans-serif !important;  background-color:#ffffff;">
+        border-color: rgb(139 139 139 / 10%); text-align: center; padding-bottom:10px; padding-top:10px; margin-bottom:0px; font-family: 'Lexend Deca', Sans-serif !important;  background-color:#ffffff; color:#000;">
 
             All rights reserved for
 
@@ -43,9 +43,9 @@ function websitinu_footer_first_page()
             © 2015
         </p>
     <?php
-    } else {
+    } elseif(get_bloginfo("language") == 'ar-OM') {
     ?>
-        <p class="footer-container text-center" style=" border-top: 1px solid rgba(255,255,255,0.15); color:rgba(255,255,255,0.8); padding-bottom:10px; padding-top:10px; margin-bottom:0px; background-color:#ffffff;">
+        <p class="footer-container text-center" style=" border-top: 1px solid rgba(255,255,255,0.15); color:rgba(255,255,255,0.8); padding-bottom:10px; padding-top:10px; margin-bottom:0px; background-color:#ffffff; color:#000;">
 
             تمامی حقوق برای
             <a href="<?php echo site_url() ?>">
@@ -64,7 +64,28 @@ function websitinu_footer_first_page()
             © 2021
         </p>
 <?php
-    }
+    } else {
+        ?>
+            <p class="footer-container text-center" style=" border-top: 1px solid rgba(255,255,255,0.15); color:rgba(255,255,255,0.8); padding-bottom:10px; padding-top:10px; margin-bottom:0px; background-color:#ffffff; color:#000;">
+    
+                تمامی حقوق برای
+                <a href="<?php echo site_url() ?>">
+                    <span style="color:#000;"><?php echo get_bloginfo(); ?>
+                    </span>
+                </a>
+                محفوظ است.
+    
+    
+                طراحی شده توسط
+                <span style="color:#000; ">
+                    <a style="color:#000;" href="https://www.websitinu.com/" target="_blank" rel="noopener">
+                        وبسایتینو
+                    </a>
+                </span>
+                © 2021
+            </p>
+    <?php
+        }
 }
 add_action('wp_footer', 'websitinu_footer_first_page');
 ?>
